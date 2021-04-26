c4edac362b527824e432a2a8bfde48d8f392e692
Magdaléna Pazlarová <m.pazlarova@gmail.com>
2021-04-26 21:56:16 +0200

    switch player

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+console.log('Funguje');
+
+let player = 'circle';
+
+const hrajeEl = document.querySelector('#naRade');
+const btnsEl = document.querySelectorAll('.hraci_pole');
+
+// Funkční verze 1
+//   const switchPayer = function (btnPressed) {
+//   btnPressed.classList.add(`board__field--${player}`);
+//   player = player === 'circle' ? 'cross' : 'circle';
+//   hrajeEl.src = `images/${player}.svg`;
+//   console.log(hrajeEl);
+//   hrajeEl.alt = `${player === 'circle' ? 'Kolečko' : 'Křížek'}`;
+// };
+
+// btnsEl.forEach((btnPressed) => {
+//   btnPressed.addEventListener('click', function () {
+//     switchPayer(btnPressed);
+//   });
+// });
+
+//Funkční verze 2 - lepší
+const myScript = function (btnPressed) {
+  btnPressed.classList.add(`board__field--${player}`);
+  btnPressed.setAttribute('disabled', '');
+  player = player === 'circle' ? 'cross' : 'circle';
+  hrajeEl.src = `images/${player}.svg`;
+  hrajeEl.style.animationDelay = '1s';
+
+  hrajeEl.alt = `${player === 'circle' ? 'Kolečko' : 'Křížek'}`;
+};
+
+btnsEl.forEach((btnPressed) => {
+  btnPressed.addEventListener('click', function () {
+    myScript(btnPressed);
+  });
+});
